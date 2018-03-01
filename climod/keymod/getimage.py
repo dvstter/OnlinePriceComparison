aa1ed4b6ad336b4418cf4e53d9ee9d33353ffdfc
dvstter <p1usj4de@163.com>
2018-03-01 11:53:15 +0800

    Preview can pull the image from jd and show the image.

--- climod/keymod/getimage.py
+++ climod/keymod/getimage.py
@@ -1,5 +1,3 @@
-import requests
-from bs4 import BeautifulSoup
 from .tools import Tools
 
 class ImageGetter:
@@ -7,8 +5,16 @@ class ImageGetter:
         pass
 
     @staticmethod
-    def get_image(item):
-        url = "http://image.baidu.com/search/index"
-        payload = {"word":item, "ie":"utf-8"}
+    def get_image(item, filename):
+        url = "https://search.jd.com/search"
+        payload = {"keyword": item, "enc": "utf-8"}
         resp, bs = Tools.request_data(url, payload)
-        Tools.debug(resp)
+        for each in bs.find_all("img", width="220", height="220"):
+            try:
+                img_src = each["src"]
+                break
+            except KeyError as _:
+                pass
+
+        img_src = img_src[2:]
+        return Tools.save_image(img_src, filename)
