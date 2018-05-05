4c6befee05bba885154a0374780454faae2ce9f4
dvstter <p1usj4de@163.com>
2018-05-05 19:16:23 +0800

    Finish crawler's functionality.

--- srvmod/crawler.py
+++ srvmod/crawler.py
@@ -1,7 +1,10 @@
+# -*- coding:utf-8 -*-
 import requests
 import random
 import time
+import json
 from bs4 import BeautifulSoup
+from srvmod.dbs import Database
 import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
@@ -19,13 +22,41 @@ class Crawler:
         self.proxy_list = None
         self.proxies = None
 
+        self.dbs = Database()
+
         self.__init_proxies()
 
-    def module_test(self):
-        categories = self.__get_category()
-        for name, url in categories:
-            print("{}:{}".format(name, url))
+    """
+    爬虫的运行函数，只需要调用这个即可更新当日的数据库
+    """
+    def run(self):
+        caturls = []
+        all_categories = self.dbs.get_all_category_names()
 
+        if all_categories:
+            for cat in all_categories:
+                url = self.dbs.get_category_url(cat)
+                caturls.append(url)
+        else:
+            categories = self.__get_categories()
+            self.__update_categories_dbs(categories)
+            caturls = [x[1] for x in categories]
+
+        for url in caturls:
+            self.__update_category_items_price(url)
+
+    """
+    用于单元测试
+    """
+    def module_test(self):
+        Crawler.Verbose = True
+        self.__update_category_items_price("http://list.jd.com/list.html?cat=12218,13581,13582")
+
+    """
+    从Proxy类中从网上搜集免费代理，因为没有找到好的免费代理，因此只有用自己的代理
+    
+    @return [(proxy_address1, proxy_port1), ...]
+    """
     def __init_proxies(self):
         '''
         # the free proxy website has been baned, so disabled these code
@@ -42,6 +73,9 @@ class Crawler:
         # use my own proxy
         return [("127.0.0.1", 1087)]
 
+    """
+    从self.proxy_list中随机挑选一个代理，并切换代理
+    """
     def __switch_proxy(self):
         try:
             ori_host, ori_port = self.proxies["http"].split(":")[1:]
@@ -64,28 +98,99 @@ class Crawler:
             if Crawler.Verbose:
                 print("Switching Proxy {}:{} ---> {}:{}".format(ori_host, ori_port, host, port))
 
-    def __request(self, url, retry=5, encoding="utf-8", max_wait=5):
+    """
+    获取某一页的数据，如果没有成功就切换代理，并再次尝试
+    
+    :param url: url
+    :param retry: 最大尝试次数
+    :param encoding: 返回页面内容的编码形式，默认是utf-8
+    
+    :return 页面的data（以encoding进行编码）
+    """
+    def __request(self, url, retry=5, encoding="utf-8"):
         try:
             return requests.get(url, headers=self.headers, proxies=self.proxies, timeout=3).content.decode(encoding)
         except Exception as _:
             if retry < 0:
                 return None
             elif retry < int(retry/2):
-                self.swith_proxy()
+                self.__switch_proxy()
                 time.sleep(random.randint(2,5))
-                return self.proxy_requests(url, retry=retry-1)
-
-    def __get_category(self):
+                return self.__request(url, retry=retry-1)
+    """
+    获取京东所有的品类及品类的url链接
+    
+    :return [(品类名1, url1), ...]
+    """
+    def __get_categories(self):
         results = []
         url = "https://www.jd.com/allSort.aspx"
         data = requests.get(url, verify=False, headers={"user-agent":self.headers["user-agent"]}).content.decode("utf-8")
-        bs = BeautifulSoup(data)
+        bs = BeautifulSoup(data, "lxml")
         for each in bs.find_all("a", target="_blank"):
             if "list.jd.com" in each["href"]:
                 results.append((each.contents[0], "http:{}".format(each["href"])))
 
         return results
 
-if __name__ == "__main__":
-    crawler = Crawler()
-    crawler.module_test()
+    def __update_categories_dbs(self, categories=None):
+        categories = self.__get_categories() if not categories else categories
+        for cat, url in categories:
+            self.dbs.add_category(cat, url)
+
+    """
+    获取某一品类某一页的所有商品和价钱并存入数据库
+    
+    :param cat_url: 类型的url
+    :param cur_page: 当前爬取的页数
+    :param pages_num: 当前品类总共的页数，第一次传入None，将会从页面中提取总页数并返回
+    
+    :return 总共的页数
+    """
+    def __update_items_price(self, cat_url, cur_page=1, pages_num=None):
+        if cur_page == 1 and Crawler.Verbose == True:
+            print("Get items from " + cat_url)
+            print("Page -------- {} --------".format(cur_page))
+
+        url = "{}&page={}&sort=sort_rank_asc&trans=1&JL=6_0_0#J_main".format(cat_url, cur_page)
+        url = url.replace("&jth=i", "")
+
+        retry = 5
+        while retry:
+            retry -= 1
+            data = self.__request(url)
+
+            if pages_num is None:
+                pages_num = data.split("<em>共<b>")[-1].split("</b>页&nbsp;&nbsp;到第</em>")[0]
+                pages_num = int(pages_num)
+                if Crawler.Verbose:
+                    print("Total {} pages".format(pages_num))
+
+            bs = BeautifulSoup(data, "lxml")
+            skuids = []
+            try:
+                for each in bs.find_all("div", class_="gl-i-wrap j-sku-item"):
+                    skuids.append(each["data-sku"])
+                    skuids = list(set(skuids))
+
+                for _id in skuids:
+                    price = json.loads(requests.get("https://p.3.cn/prices/mgets?skuIds=J_" + _id).text)[0]["p"]
+                    self.dbs.add_price_another_day(_id, price)
+                    print("Added {} price {}".format(_id, price))
+                    time.sleep(random.randint(2,10)/10.0)
+            except IndexError as ie:
+                print(ie)
+
+        return pages_num
+
+    def __update_category_items_price(self, cat_url, wait_circle=None):
+        cur_page = 1
+        pages_num = None
+        while True:
+            pages_num = self.__update_items_price(cat_url, cur_page, pages_num)
+            cur_page += 1
+            if cur_page > pages_num:
+                break
+            if wait_circle:
+                time.sleep(wait_circle)
+
